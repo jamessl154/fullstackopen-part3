6e68ca8a2af72d8b48e30e984a8126d8176e8ac8
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-09-13 17:41:58 +0100

    part3 phonebook backend step3 done

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(x => x.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 app.get('/info', (request, response) => {
     const number = persons.length
     response.send(`<p>Phonebook has info for ${number} people</p> <p>${new Date()}</p>`)
