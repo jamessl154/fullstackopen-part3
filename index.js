54c2dc1781ad4adac5f2f8aeb2153ad490b6b729
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-09-14 15:03:11 +0100

    part3 phonebook backend step9 done, added procfile

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', function(req, res) { 
-  return JSON.stringify(req.body)
+  return JSON.stringify(req.body, null, 2)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -89,7 +91,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
